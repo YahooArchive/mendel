f6a4276b42f0b95b7546a692f51682f9a6b083c1
Irae <irae@irae.pro.br>
2016-03-03 20:32:13 -0800

    Promote requirify to it's own plugin, works standalone

--- packages/mendel-browserify/index.js
+++ packages/mendel-browserify/index.js
@@ -12,7 +12,6 @@ var validVariations = require('./lib/variations');
 var variationMatches = require('./lib/variation-matches');
 var proxy = require('./lib/proxy');
 var onlyPublicMethods = proxy.onlyPublicMethods;
-var requirify = require('../mendel-requirify');
 
 module.exports = mendelBrowserify;
 
@@ -46,10 +45,6 @@ function mendelBrowserify(baseBundle, opts) {
         createManifest(
             baseBundle, baseBundle, opts, variationsWithBase, baseVariation
         );
-    }
-
-    if (opts.writefiles) {
-        writeFiles(baseBundle, opts);
     }
 
     if (baseBundle.argv.o || baseBundle.argv.outfile) {
@@ -86,10 +81,6 @@ function mendelBrowserify(baseBundle, opts) {
                 createManifest(
                     bv, baseBundle, opts, variationsWithBase, variation
                 );
-            }
-
-            if (opts.writefiles) {
-                writeFiles(bv, opts);
             }
         });
     });
@@ -136,22 +127,6 @@ function createManifest(bundle, baseBundle, opts, variations, variation) {
         }
     })});
 }
-
-var writeFiles = (function () {
-    var written = {};
-
-    return function writeFiles(bundle, opts) {
-        var basedir = path.dirname(opts.basetree);
-        var outdir = path.join(opts.outdir, 'server');
-
-        bundle.pipeline.get('deps').push(requirify({
-            from: basedir,
-            to: outdir,
-            exclude: [ 'node_modules' ],
-            cache: written
-        }));
-    }
-})();
 
 function pushBundleManifest(baseBundle, dep) {
     var id = dep.id;
--- packages/mendel-requirify/index.js
+++ packages/mendel-requirify/index.js
@@ -1,37 +1,27 @@
 var fs = require('fs-extra');
+var path = require('path');
 var through = require('through2');
 var mendelRequireTransform = require('./lib/mendel-require-transform');
-var changeRoot = require('./lib/change-root');
 
-function isExcluded(file, filters) {
-    return filters.some(function (pattern) {
-        return pattern.test(file);
-    });
-}
+function requirify(b, opts) {
+    var bundleOptions = b._options || {};
+    var basedir = bundleOptions.basedir || process.cwd();
+    var outdir = opts.outdir || path.join(process.cwd(), 'mendel-requirefy');
 
-function asRegExp(pattern) {
-    return new RegExp(pattern);
-}
-
-function requirify(opts) {
-    var written = opts.cache || {};
-    var filters = opts.exclude || [];
-
-    filters = filters.map(asRegExp);
-
-    return through.obj(function(row, enc, next) {
-        var file = row.file;
-        if (file && !written[file]) {
-            if (!isExcluded(file, filters)) {
-                var dest = changeRoot(file, opts.from, opts.to);
-                var out = fs.createOutputStream(dest);
-                out.write(mendelRequireTransform(row.source, true));
-                out.end();
-                written[file] = true;
-            }
+    b.pipeline.get('dedupe').push(through.obj(function(row, enc, next) {
+        var file = row.file || row.id;
+        var nm = file.split('/node_modules/')[1];
+        if (!nm) {
+            file = path.relative(basedir, file);
+            var dest = path.join(outdir, file);
+            var out = fs.createOutputStream(dest);
+            out.write(mendelRequireTransform(row.source, true));
+            out.end();
         }
+        this.push(row);
         next();
-    });
+    }));
+
 }
 
 module.exports = requirify;
