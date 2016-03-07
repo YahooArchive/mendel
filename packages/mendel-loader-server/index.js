182716d510c8a3c4a5b35173946e4cbf3496a1be
David Gomez <dgomez@yahoo-inc.com>
2016-03-07 15:51:27 -0800

    Use MendelTrees to resolve variations.

--- packages/mendel-loader-server/index.js
+++ packages/mendel-loader-server/index.js
@@ -1,7 +1,6 @@
 var path = require('path');
 var Module = require('module');
-var mendelConfig = require('mendel/config');
-var variations = require('mendel/lib/variations');
+var MendelTree = require('mendel/trees');
 
 var nodeRequire = module.constructor.prototype.require;
 
@@ -11,52 +10,28 @@ function MendelLoader(config) {
     var self = this;
     self._basedir = config.basedir || process.cwd();
     self._mountdir = config.mountdir || process.cwd();
-    self._mendelConfig = mendelConfig(self._basedir);
-    self._variations = variations(self._mendelConfig).reduce(function (acc, v) {
-        acc[v.id] = v.chain;
-        return acc;
-    }, {});
-    self._manifest = require(path.resolve(config.manifest));
+    self._tree = new MendelTree({
+        basedir: self._basedir
+    });
 }
 
 MendelLoader.prototype.resolver = function(context) {
-    return new MendelResolver(this, context);
+    var variations = this._getVariationMap(context);
+    return new MendelResolver(variations, this._mountdir);
 }
 
 MendelLoader.prototype.register = function(context) {
     return this.resolver(context).register();
 }
 
-MendelLoader.prototype.getChain = function(variationId) {
-    return this._variations[variationId] || [];
+MendelLoader.prototype._getVariationMap = function(context) {
+    //TODO(dgomez): this tree could be cached.
+    return this._tree.findTreeForVariations('main', context).variationMap;
 }
 
-MendelLoader.prototype.getVariation = function(mod, id) {
-    var variation;
-    var bIdx = this._manifest.indexes[mod];
-
-    if (bIdx !== undefined) {
-        var bundle = this._manifest.bundles[bIdx];
-        var vIdx = bundle.variations.indexOf(id);
-        if (vIdx > -1) {
-            variation = bundle.data[vIdx].variation;
-        }
-    }
-    return variation;
-}
-
-MendelLoader.prototype.getBaseDir = function() {
-    return this._basedir;
-}
-
-MendelLoader.prototype.getMountDir = function() {
-    return this._mountdir;
-}
-
-function MendelResolver(loader, context) {
-    this._loader = loader;
-    this._mountdir = loader.getMountDir();
-    this._context = context;
+function MendelResolver(variations, mountdir) {
+    this._variations = variations;
+    this._mountdir = mountdir;
     this._resolveCache = {};
 }
 
@@ -76,25 +51,10 @@ MendelResolver.prototype.unregister = function() {
 
 MendelResolver.prototype.resolve = function(mod) {
     if (!this._resolveCache[mod]) {
-        var variation = this._getVariation(mod);
+        var variation = this._variations[mod];
         if (variation) {
             this._resolveCache[mod] = path.resolve(path.join(this._mountdir, variation, mod));
         }
     }
     return this._resolveCache[mod];
-}
-
-MendelResolver.prototype._getVariation = function(mod) {
-    var ctxVariations = this._context.variations;
-    for (var i = 0; i < ctxVariations.length; i++) {
-        var v = ctxVariations[i];
-        var chain = this._loader.getChain(v);
-        for (var j = 0; j < chain.length; j++) {
-            var c = chain[j];
-            var variation = this._loader.getVariation(mod, c);
-            if (variation) {
-                return variation;
-            }
-        }
-    }
 }
