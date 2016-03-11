56c0ec1d95024057a3d0a437e054540adb7c58ed
David Gomez <dgomez@yahoo-inc.com>
2016-03-11 11:44:54 -0800

    Refactored regex.

--- test/require-transform.js
+++ test/require-transform.js
@@ -1,15 +1,18 @@
 var t = require('tap');
 var requireTransform = require('../lib/require-transform');
 
+var mendelRequireRegexp = /__mendel_require__\(/;
+var requireRegexp = /\brequire\(/;
+
 var src = "var foo = require('bar');";
 
 var out = requireTransform(src, false);
-t.match(out, /__mendel_require__\(/);
-t.notMatch(out, /\brequire\(/);
+t.match(out, mendelRequireRegexp);
+t.notMatch(out, requireRegexp);
 
 out = requireTransform(src, true);
-t.match(out, /__mendel_require__\(/);
-t.notMatch(out, /\brequire\(/);
+t.match(out, mendelRequireRegexp);
+t.notMatch(out, requireRegexp);
 
 var wrapper = requireTransform.wrapper;
 t.equal(out.indexOf(wrapper[0]), 0, 'wrapper prelude pos');
