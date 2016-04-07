6043127dad5ce6a99e71949cc455289f492b7091
Irae <irae@irae.pro.br>
2016-04-06 20:13:51 -0700

    Add watchify with cache to middleware

--- packages/mendel-middleware/index.js
+++ packages/mendel-middleware/index.js
@@ -1,12 +1,14 @@
 
 var express = require('express');
-
-
+var StreamCache = require('stream-cache');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var path = require('path');
+var xtend = require('xtend');
 
 var parseConfig = require('./lib/config');
 var validVariations = require('./lib/variations');
-
-
+var treenherit = require('mendel-treenherit');
 
 module.exports = MendelMiddleware;
 
@@ -15,14 +17,14 @@ function MendelMiddleware(opts) {
     var config = parseConfig(opts);
     var existingVariations = validVariations(config);
     var base = config.base || 'base';
-    var path = '/mendel/:variations/:bundle\.js' || opts.path;
+    var routePath = '/mendel/:variations/:bundle\.js' || opts.path;
 
     existingVariations = existingVariations.concat({
         id: base,
         chain: [config.basetree || 'base'],
     })
 
-    return router.get(path, function(req, res, next) {
+    return router.get(routePath, function(req, res, next) {
         var variations = (req.params.variations||'').split(',').concat(base);
         var bundle = req.params.bundle;
 
@@ -33,14 +35,31 @@ function MendelMiddleware(opts) {
             return next();
         }
 
-        return res.end(JSON.stringify({
-            bundle:bundle,
-            variations:variations,
-            bundleConfig:bundleConfig,
-            dirs:dirs,
-            existingVariations:existingVariations,
-            config:config
-        }, null, '  '));
+        // return res.end(JSON.stringify({
+        //     bundle:bundle,
+        //     variations:variations,
+        //     bundleConfig:bundleConfig,
+        //     dirs:dirs,
+        //     existingVariations:existingVariations,
+        //     config:config
+        // }, null, '  '));
+
+        res.header('content-type', 'application/javascript');
+
+        bundleConfig = xtend(bundleConfig, {
+            base: base,
+            basedir: config.basedir,
+            cache: {},
+            packageCache: {}
+        });
+
+        var bundleStream = cachedStreamBundle(bundleConfig, dirs);
+        bundleStream.on('error', function(e) {
+            console.error(e.stack);
+            res.send('console.error( "Error compiling client bundle", ' +
+                JSON.stringify({ stack: e.stack }) + ')').end();
+        });
+        bundleStream.pipe(res);
     });
 }
 
@@ -61,3 +80,45 @@ function resolveVariations(existingVariations, variations) {
     return resolved;
 }
 
+var streamCache = {};
+function cachedStreamBundle(bundleConfig, dirs) {
+    var id = [bundleConfig.id].concat(dirs).join('/');
+    if (!streamCache[id]) {
+        streamCache[id] = new StreamCache();
+        getCachedWatchfy(id, bundleConfig, dirs, function(err, bundler) {
+            function bundleError(e) {
+                streamCache[id].emit('error', e);
+                delete streamCache[id];
+            }
+            if (err) return bundleError(err);
+            var bundle = bundler.bundle();
+            bundle.on('error', bundleError);
+            bundle.pipe(streamCache[id]);
+        });
+    }
+
+    return streamCache[id];
+}
+
+var watchfyCache = {};
+function getCachedWatchfy(id, bundleConfig, dirs, cb) {
+    if (watchfyCache[id]) {
+        return cb(null, watchfyCache[id]);
+    }
+
+    // TODO: async lookup of entries
+    bundleConfig.entries = bundleConfig.entries.map(function(entry) {
+        return path.join(bundleConfig.base, entry);
+    });
+
+    var bundler = browserify(bundleConfig);
+    bundler.transform(treenherit, { dirs: dirs });
+    bundler.plugin(watchify);
+
+    bundler.on('update', function() {
+        streamCache[id] = null;
+    });
+
+    watchfyCache[id] = bundler;
+    cb(null, watchfyCache[id]);
+}
