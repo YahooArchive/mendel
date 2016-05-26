6b82a24ccedfad56c3c81c41899c60d8f54dd89e
Irae Carvalho <irae@irae.pro.br>
2016-05-26 16:37:32 -0700

    Remove all uses of isarray (#28)

--- test/app-samples/1/app/number-list.js
+++ test/app-samples/1/app/number-list.js
@@ -1,7 +1,6 @@
-var isArray = require('isarray');
 var oneNumber = require('./some-number')
 
 module.exports = function() {
     var a = [oneNumber()];
-    return isArray(a) ? a : [];
+    return Array.isArray(a) ? a : [];
 };
