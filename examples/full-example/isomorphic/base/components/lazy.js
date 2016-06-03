370b2e4a477749c949ea4925c36d1045111e9673
Devrim Tufan <tufandevrim@gmail.com>
2016-06-02 18:56:48 -0700

    Add lazy button into the example (#30)

---
+++ examples/full-example/isomorphic/base/components/lazy.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from './button';
+
+class Lazy extends React.Component {
+    render() {
+        return <Button>Button Lazy</Button>
+    }
+}
+
+export default Lazy;
