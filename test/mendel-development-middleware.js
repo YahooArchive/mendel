03f88cd50d2c165f075a9f33be4c06d1a51fbc2b
Irae <irae@irae.pro.br>
2016-08-24 19:24:38 -0700

    Tests for development middleware

--- test/mendel-development-middleware.js
+++ test/mendel-development-middleware.js
@@ -4,25 +4,52 @@ var sut = require('../packages/mendel-development-middleware');
 var express = require('express');
 var request = require('request');
 var path = require('path');
+var async = require('async');
 
 var app = express();
 app.use(sut({
-    basedir: path.join(__dirname, './config-samples')
+    basedir: path.join(__dirname, './app-samples/1/')
 }));
 
 var server = app.listen('1337');
 var host = 'http://localhost:1337';
+var appBundle = host+'/mendel/app/app.js';
 tap.tearDown(function() {
     server.close(process.exit);
 });
 
-tap.test('mendel-development-middleware', function(t){
+tap.test('mendel-development-middleware serves a bundle', function(t){
+    t.plan(2);
+
+    request(appBundle, function(error, response, body) {
+        if (error) t.bailout(error);
+
+        t.match(response, {
+            statusCode: 200,
+            headers: { 'content-type': 'application/javascript' }
+        }, 'serves javascript');
+        t.contains(body, 'sourceMappingURL=data:application/json;',
+            'has source maps');
+    });
+});
+
+tap.test('serves cached bundles', function(t) {
     t.plan(1);
 
-    var vendor = host+'/mendel/name_for_base_variation/vendor.js';
-    request(vendor, function(error) {
-        if (error) t.bail(error);
+    function getVendor(done) {
+        request(appBundle, function(error) {
+            done(error);
+        });
+    }
 
-        t.pass('serves javascript');
+    // this would cause test to timeout if cache is disabled
+    var requests = [];
+    for (var i = 0; i < 1000; i++) {
+        requests.push(getVendor);
+    }
+    async.series(requests, function(error) {
+        if (error) t.bailout(error);
+
+        t.pass('got 1000 of the same bundle');
     });
 });
