d097801664cd317c2ac810af1218f6bf614faf1e
Irae <irae@irae.pro.br>
2016-10-27 14:58:06 -0700

    More fully featured manifest-extract-bundles

--- packages/mendel-manifest-extract-bundles/manifest-extract.js
+++ packages/mendel-manifest-extract-bundles/manifest-extract.js
@@ -10,57 +10,142 @@ function manifestExtractBundles(manifests, options, next) {
     var fromManifest = manifests[options.from];
     var externalManifest = manifests[options.external];
 
-    var fromFiles = Object.keys(fromManifest.indexes);
-    var externalFiles = Object.keys(externalManifest.indexes);
+    var fromFiles = function() {
+        return Object.keys(fromManifest.indexes);
+    };
+    var externalFiles = function() {
+        return Object.keys(externalManifest.indexes);
+    };
 
-    var intersection = externalFiles.filter(function(file) {
-        return fromFiles.indexOf(file) >= 0;
+    // Modules that are exposed on "external" should be removed from main
+    filterFilesFromManifest(fromManifest, function(file) {
+        var modIndex = externalFiles().indexOf(file);
+        var isExposedOnExternalBundle = false;
+        if (-1 !== modIndex) {
+            var externalMod = externalManifest.bundles[modIndex];
+            if (externalMod.expose) {
+                isExposedOnExternalBundle = true;
+            }
+        }
+        return !isExposedOnExternalBundle;
+    }, options.from);
+
+    // Cleanup potetial sub-dependencies of removed files
+    removeOrphans(fromManifest, options.from);
+
+    // Files that exist on main should be removed from external
+    var removedFromExternal = [];
+    filterFilesFromManifest(externalManifest, function(file) {
+        if (fromFiles().indexOf(file) >= 1) {
+            removedFromExternal.push(file);
+            return false;
+        }
+        return true;
+    }, options.external);
+
+    // Cleanup potetial sub-dependencies of removed files
+    removeOrphans(externalManifest, options.external);
+
+    // Files that exist on main and were removed from external bundle
+    // should be exposed on main
+    fromFiles().forEach(function(file) {
+        var moduleIndex = fromManifest.indexes[file];
+        var bundle = fromManifest.bundles[moduleIndex];
+        if (removedFromExternal.indexOf(file) > -1) {
+            bundle.expose = bundle.id;
+            bundle.variations.forEach(function(variation, index) {
+                bundle.data[index].expose = bundle.id;
+            });
+            debug(bundle.id + ' exposed on ' + options.from);
+        }
     });
 
     debug([
-        'found', intersection.length, 'files intersecting between',
-        options.from, 'and', options.external
+        fromFiles().length, 'files remaining in',
+        options.from, 'after extraction',
+    ].join(' '));
+    debug([
+        externalFiles().length, 'files remaining in',
+        options.external, 'after extraction',
     ].join(' '));
 
-    // Expose files on the source bundle
-    intersection.forEach(function(file) {
-        var moduleIndex = fromManifest.indexes[file];
-        var bundle = fromManifest.bundles[moduleIndex];
-        bundle.expose = bundle.id;
-        bundle.variations.forEach(function(variation, index) {
-            bundle.data[index].expose = bundle.id;
-        });
-    });
+    next(manifests);
+}
 
-    // Make modules external on the external bundle
+/*
+receives a manifest and a "selector" function
+modifies manifest indexes to include only files that selector returns truthy
+*/
+function filterFilesFromManifest(manifest, selector, logName) {
     // By removing files from the index, when the manifest is sorted and
     // validated, unreachable modules are removed.
-    externalManifest.indexes = externalFiles.reduce(function(indexes, file) {
-        if (-1 === intersection.indexOf(file)) {
-            indexes[file] = externalManifest.indexes[file];
+    var removedFiles = [];
+    manifest.indexes = Object.keys(manifest.indexes)
+    .reduce(function(indexes, file) {
+        if (selector(file)) {
+            indexes[file] = manifest.indexes[file];
+        } else {
+            removedFiles.push(file);
+            debug(file + ' removed from ' + logName);
         }
         return indexes;
     }, {});
 
     // Also, we need to make sure deps are updated to "false", which marks
     // the dep as external
-    Object.keys(externalManifest.indexes).forEach(function(file) {
-        var bundle = externalManifest.bundles[externalManifest.indexes[file]];
+    Object.keys(manifest.indexes).forEach(function(file) {
+        var bundle = manifest.bundles[manifest.indexes[file]];
         bundle.data.forEach(function(module) {
             Object.keys(module.deps).forEach(function(key) {
                 var value = module.deps[key];
-                if (intersection.indexOf(value) >=0) {
+                if (removedFiles.indexOf(value) >=0) {
                     module.deps[key] = false;
                 }
             });
         });
     });
-
-    var remaining = Object.keys(externalManifest.indexes).length;
-    debug([
-        remaining, 'files remaining in',
-        options.external, 'after extraction'
-    ].join(' '));
-
-    next(manifests);
 }
+
+/*
+walks the manifest dependencies and remove indexes that are unreachable
+*/
+function removeOrphans(manifest, logName) {
+    var originalIndexes = Object.keys(manifest.indexes);
+
+    // hashtable for number of time a bundle was visited
+    var visitedIndexes = originalIndexes.reduce(function(indexes, file) {
+        indexes[file] = false;
+        return indexes;
+    }, {});
+
+    // recursive function to visit bundle and it's deps
+    function visitBundle(file) {
+        if (visitedIndexes[file]) {
+            return;
+        }
+        visitedIndexes[file] = true;
+        // walk deps
+        var bundle = manifest.bundles[manifest.indexes[file]];
+        bundle.data.forEach(function(module) {
+            Object.keys(module.deps).forEach(function(key) {
+                var value = module.deps[key];
+                if (visitedIndexes.hasOwnProperty(value)) {
+                    visitBundle(value);
+                }
+            });
+        });
+    }
+
+    // start visiting from entries and exposed
+    originalIndexes.forEach(function(file) {
+        var bundle = manifest.bundles[manifest.indexes[file]];
+        if (bundle.expose || bundle.entry) {
+            visitBundle(file);
+        }
+    });
+
+    filterFilesFromManifest(manifest, function(file) {
+        return visitedIndexes.hasOwnProperty(file) && visitedIndexes[file];
+    }, logName);
+}
+
