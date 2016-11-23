ad2600e2ff069ea82f8840f2f4ec4a543b122a03
Stephan Lee <stephanwlee@gmail.com>
2016-11-23 11:29:33 -0800

    Read file in appropriate encoding according to type

--- packages/mendel-pipeline/src/step/fs-reader/index.js
+++ packages/mendel-pipeline/src/step/fs-reader/index.js
@@ -5,19 +5,27 @@ const analytics = require('../../helpers/analytics/analytics')('fs');
 const debugError = require('debug')('mendel:reader:error');
 
 class FileReader extends BaseStep {
-    constructor({registry}, {projectRoot}) {
+    constructor({registry}, {projectRoot, types}) {
         super();
 
         this.projectRoot = projectRoot;
         this.registry = registry;
+        this._typeMap = new Map();
+        types.forEach(type => this._typeMap.set(type.name, type));
         this.sourceExt = new Set();
     }
 
     perform(entry) {
         const filePath = path.resolve(this.projectRoot, entry.id);
+        const entryType = this.registry.getType(entry.id);
+        const isBinary = !this._typeMap.has(entryType) || this._typeMap.get(entryType).isBinary;
+
+        if (entryType === 'virtual') {
+            return this.emit('done', {entryId: entry.id});
+        }
 
         analytics.tic('read');
-        fs.readFile(filePath, 'utf8', (error, source) => {
+        fs.readFile(filePath, isBinary ? 'binary' : 'utf8', (error, source) => {
             analytics.toc('read');
             if (error) {
                 debugError(`Errored while reading ${filePath}`);
