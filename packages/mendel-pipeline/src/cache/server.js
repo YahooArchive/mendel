9f16a51ac79f7853f568c71c69664591449cc852
Irae <irae@irae.pro.br>
2016-12-02 00:50:58 -0800

    Small fix

--- packages/mendel-pipeline/src/cache/server.js
+++ packages/mendel-pipeline/src/cache/server.js
@@ -24,7 +24,7 @@ class CacheServer extends EventEmitter {
         this.server = network.getServer(config.cacheConnection);
         this.initServer();
 
-        debug('listening', config.cachePort);
+        debug('listening', config.cacheConnection);
     }
 
     close() {
