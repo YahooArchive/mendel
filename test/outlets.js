6e5270232e620fbd1e34cd8381fecf40cbd8d6d6
Stephan Lee <stephanwlee@gmail.com>
2016-12-29 15:31:29 -0800

    Improved outlet test to use programmatic Mendelv2

--- test/outlets.js
+++ test/outlets.js
@@ -1,16 +1,16 @@
-
 const path = require('path');
 const test = require('tap').test;
-const exec = require('child_process').exec;
-
+const MendelV2 = require('../packages/mendel-pipeline');
 const appPath = path.join(__dirname, '../examples/full-example/');
 
 test('Mendel v2 outlets sanity test', function (t) {
     t.plan(2);
 
     process.chdir(appPath);
+    process.env.MENDELRC = '.mendelrc_v2';
 
-    exec('./mendel-v2.sh', { cwd: appPath }, function(error) {
+    const mendel = new MendelV2();
+    mendel.run(function(error) {
         if (error) {
             console.error(error);
             return t.bailout('should create manifest but failed');
@@ -22,16 +22,15 @@ test('Mendel v2 outlets sanity test', function (t) {
             'manifest has minimal attributes');
 
         const toolbar = manifest.bundles[
-            manifest.indexes["./components/toolbar"]
+            manifest.indexes['./components/toolbar']
         ];
         const pCVariation = toolbar.data[
-            toolbar.variations.indexOf("isomorphic/variations/partner_C")
+            toolbar.variations.indexOf('isomorphic/variations/partner_C')
         ];
         t.matches(pCVariation.deps, {
             react: './node_modules/react/react.js',
             './button': './components/button',
             './dropdown': './components/dropdown',
         }, 'correct variations on conplex scenario');
-
     });
 });
