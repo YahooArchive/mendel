0f8ff2245cd432b86c5e321c18a32f6501c583b5
Stephan Lee <stephanwlee@gmail.com>
2017-06-03 16:48:04 -0400

    Prevent requiring in transformer in Mendel-config
    Because of "require" (another option is to use AST parser),
    for instance of transform-babel, we required all of babel which
    contributed to large bootstrap time on low-powered computer.

--- packages/mendel-config/src/transform-config.js
+++ packages/mendel-config/src/transform-config.js
@@ -1,18 +1,32 @@
 var createValidator = require('./validator');
 var nodeResolveSync = require('resolve').sync;
+var path = require('path');
+
+function _resolve(pluginPath, opt) {
+    try {
+        return nodeResolveSync(pluginPath, opt);
+    } catch (e) {
+        return false;
+    }
+}
+
 
 function TransformConfig(id, transform, {projectRoot}) {
     this.id = id;
+    const basedir = projectRoot;
+    const pluginPackagePath = _resolve(path.join(transform.plugin, 'package.json'), {basedir});
+    const pluginPath = _resolve(transform.plugin, {basedir});
 
-    try {
-        this.plugin = nodeResolveSync(transform.plugin, {basedir: projectRoot});
-        this.mode = require(this.plugin).mode || 'ist';
-    } catch (e) {
-        if (e.code !== 'MODULE_NOT_FOUND') throw e;
-        this.plugin = false;
+    if (pluginPath) {
+        this.mode = [pluginPackagePath, pluginPath]
+            .filter(Boolean)
+            .map(plugin => require(plugin).mode)
+            .filter(Boolean)[0] || 'ist';
+    } else {
         this.mode = 'unknown';
     }
 
+    this.plugin = pluginPath;
     this.options = transform.options;
 
     TransformConfig.validate(this);
